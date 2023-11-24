8f63b0b313c89d463564cbc6c29824ae068e5366
anlgncr <anlgncr@gmail.com>
2023-11-24 15:25:28 +0300

    Update WS0010.cpp

--- WS0010/WS0010.cpp
+++ WS0010/WS0010.cpp
@@ -1,6 +1,14 @@
 #include "WS0010.h"
 #include "Arduino.h"
-
+/*
+ * This library does not use SPI Hardware
+ * because we need to send 10 bits exactly
+ *
+ * 10 -> Chip Select
+ * 11 -> Serial Data Out
+ * 12 -> Serial Data Input
+ * 13 -> Serial Clock
+*/
 
 WS0010::WS0010(){
 	PORTB |= B00101100;
@@ -14,10 +22,20 @@ void WS0010::init(){
 	sendCmd(CHAR_MODE_PWR_ON);
 	sendCmd(TURN_ON_DISPLAY);
 	sendCmd(ENTRY_MODE);
+	
+	uploadTurkishChars();
 	sendCmd(RETURN_HOME);
 }
 
-void WS0010::clear(){
+void WS0010::showCursor(){
+	sendCmd(BLINK_AND_CURSOR);
+}
+
+void WS0010::hideCursor(){
+	sendCmd(TURN_ON_DISPLAY);
+}
+
+void WS0010::clear(){	
 	sendCmd(CLEAR_DISPLAY);
 	sendCmd(RETURN_HOME);
 }
@@ -26,12 +44,13 @@ void WS0010::moveCursor(uint8_t row, uint8_t col){
 	row = (row > 0)? 1 : 0;
 	col = (col > 63)? 63 : col;
 	
-	uint8_t position = row * 64 + col; 
-	sendCmd(0x80 | position);
+	byte cursorPos = row * 64 + col; 
+	sendCmd(SET_DDRAM_ADDR | cursorPos);
 }
 
-uint8_t WS0010::isBusy(){
-	PORTB &= B00111011;														// digitalWrite(10, LOW);
+uint8_t WS0010::isBusy()
+{
+	PORTB &= B00111011;
 	
 	uint8_t data = 0x80;
 	while((0x80 & data))
@@ -46,40 +65,28 @@ uint8_t WS0010::isBusy(){
 			data |= digitalRead(12);
 		}
 	}
-	PORTB |= B00000100; 													// digitalWrite(10, HIGH);
+	PORTB |= B00000100;
 	return data;
 }
 
 void WS0010::sendCmd(uint8_t cmd){
 	isBusy();
 	
-	PORTB &= B00111011;														// digitalWrite(10, LOW);
+	PORTB &= B00111011;
 	send(0,2);
 	send(cmd, 8);
-	PORTB |= B00000100; 													// digitalWrite(10, HIGH);
+	PORTB |= B00000100;
 }
 
 void WS0010::sendData(uint8_t *data, uint8_t length){
-	PORTB &= B00111011;														// digitalWrite(10, LOW);
+	PORTB &= B00111011;
 	send(0x80,2);
 	
 	for(uint8_t i=0; i<length; i++){
 		send(data[i], 8);
 	}
 	
-	PORTB |= B00000100; 													// digitalWrite(10, HIGH);
-}
-
-void WS0010::print(char *string)
-{
-	PORTB &= B00111011;														// digitalWrite(10, LOW);
-	send(0x80,2);
-	
-	while(*string != '\0'){
-		send(*string++, 8);
-	}
-	
-	PORTB |= B00000100; 													// digitalWrite(10, HIGH);
+	PORTB |= B00000100;
 }
 
 /* MSB FIRST */
@@ -88,13 +95,80 @@ void WS0010::send(uint8_t data, uint8_t length)
 	uint8_t mask = 0x80;
 	for(uint8_t i=0; i<length; i++)
 	{		
-		if(data & mask)														// digitalWrite(11, data & mask);
+		if(data & mask)
 			PORTB |= B00001000;		
 		else 						
 			PORTB &= B11110111;
 		
-		PORTB &= B11011111;													// digitalWrite(13, LOW);
-		PORTB |= B00100000;													// digitalWrite(13, HIGH);
+		PORTB &= B11011111;
+		PORTB |= B00100000;
 		mask = mask >> 1;
 	}
+}
+
+void WS0010::uploadTurkishChars(){
+	sendCmd(SET_CGRAM_ADDR);
+	
+	PORTB &= B00111011;
+	send(0x80,2);
+	
+	for(uint8_t i=0; i<64; i++){
+		send(pgm_read_byte(&turkishChars[i]), 8);
+	}
+	PORTB |= B00000100;
+}
+
+void WS0010::print(char* text)
+{
+	PORTB &= B00111011;
+	send(0x80, 2);
+	
+	while(*text != '\0')
+	{
+		uint8_t byteCount = 0;
+		for(uint8_t i=0; i<5; i++)
+		{
+			if(*text & (0x80 >> i))
+				byteCount++;
+			else
+				break;
+		}
+		for(byte i=0; i<byteCount; i++){
+			byte b = *(text + i);
+			Serial.println(b, BIN);
+		}
+		if(byteCount == 0){
+			byteCount = 1;
+		}
+		
+		char newVal = 0x07; // Bilinmeyen karakterler için gülen surat 
+		if(byteCount == 2)
+		{
+			uint16_t unicodedChar = *text & 0x1F;
+			unicodedChar <<= 6;
+			uint8_t secondByte = text[1] & 0x3F;
+			unicodedChar |= secondByte;
+			
+			switch(unicodedChar){
+				case 0x00E7: newVal = 0x87; break; //ç
+				case 0x011F: newVal = 0x00; break; //ğ
+				case 0x0131: newVal = 0x01; break; //ı
+				case 0x00F6: newVal = 0x94; break; //ö
+				case 0x015F: newVal = 0x02; break; //ş
+				case 0x00FC: newVal = 0x81; break; //ü
+				case 0x00C7: newVal = 0x80; break; //Ç
+				case 0x0130: newVal = 0x03; break; //İ
+				case 0x011E: newVal = 0x04; break; //Ğ
+				case 0x00D6: newVal = 0x05; break; //Ö
+				case 0x015E: newVal = 0x06; break; //Ş
+				case 0x00DC: newVal = 0x9A; break; //Ü
+			}                                        
+		}
+		else{
+			newVal = *text;
+		}
+		send(newVal, 8);
+		text += byteCount;
+	}
+	PORTB |= B00000100;
 }
